90a429383661493d5c84c6e2be985246b180e666
wadebooth <wademgmt@gmail.com>
2022-05-03 16:16:10 -0400

    set up restaurants database

---
+++ src/restaurants.js
@@ -0,0 +1,27 @@
+import connectDb from "./connectDb.js";
+
+export const addRestaurant = async (req, res) => {
+  //check if request is valid
+  if (!req.body || !req.body.name || !req.body.address) {
+    res.status(401).send("Invalid request");
+    return;
+  }
+  // connect to Firestore
+  const db = connectDb();
+  // prepare the data
+  const newRestaurant = {
+    name: req.body.name,
+    address: req.body.address,
+    rating: req.body.rating || 3,
+    cuisine: req.body.cuisine || "American",
+  };
+  // add data to the restaurants collection
+  try {
+    const doc = await db.collection("restaurants").add(newRestaurant);
+    // respond with success
+    res.status(201).send("Restaurant created " + doc.id);
+  } catch (err) {
+    // respond with error
+    res.status(500).send(err);
+  }
+};
